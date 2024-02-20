600f6d9ecf5298fa56cbeec40070e9e39e33c503
GabyIsBored <gabrielcheikhad@gmail.com>
2024-02-20 19:20:39 +0200

    dash sans foncs 2

--- sources/dashboard.py
+++ sources/dashboard.py
@@ -31,7 +31,15 @@ h1Font = ('Inter', 34, "bold")
 h3Font = ('Inter', 20, "bold")
 pFont = ('Inter', 9, "bold")
 
-#backgroundColor=mainFrame['background']
+s = tb.Style()
+s.configure('My.TFrame', background='white')
+
+i=0
+chapitres=['Introduction\n','Variables \net affectations','Arithmetique \net comparaisons','Conditions\n','Boucles\n',
+		   'Boucles avancees\n','Fonctions\n','Tableaux\n','Dictionnaires \net Tuples','‘Import’ \net Modules']
+progressions=[0,0,0,0,0,0,0,0,0,0]
+
+
 
 
 # Creating grid
@@ -99,26 +107,32 @@ dashboard = tb.Canvas(mainFrame, width=dashboardImg.width(), height=dashboardImg
 dashboard.create_image(0, 0, anchor=NW, image=dashboardImg)
 dashboard.grid(row=0,column=0,columnspan=7,sticky='ns',pady=(50,0))  
 
+# LOGIQUE FLECHE 
+
+def move(n):
+	global i
+	i=i+n
+	if i<=len(chapitres)-3 and i>=0:
+		Chapter(mainFrame,1,1,chapitres[i],progressions[i])
+		Chapter(mainFrame,3,1,chapitres[i+1],progressions[i+1])
+		Chapter(mainFrame,5,1,chapitres[i+2],progressions[i+2])
+	else:
+		i=i-n
 
 # FLECHE GAUCHE
-fleche_g=Button(mainFrame,bg="#002b36",image=fleche_gImg)
+fleche_g=Button(mainFrame,bg="#002b36",image=fleche_gImg,command=lambda:move(-1))
 fleche_g.configure(bg= "#002b36")
 fleche_g.grid(row=1,column=0) 
 
 # FLECHE DROITE
 
-fleche_d=Button(mainFrame,bg="#002b36",image=fleche_dImg)
+fleche_d=Button(mainFrame,bg="#002b36",image=fleche_dImg,command=lambda:move(1))
 fleche_d.configure(bg= "#002b36")
 fleche_d.grid(row=1,column=6) 
 
-
-
-s = tb.Style()
-s.configure('My.TFrame', background='white')
-
 # CHAPITRE
 class Chapter(tb.Frame):
-	def __init__(self, parent,columnNum,rowNum,chapName,new):
+	def __init__(self, parent,columnNum,rowNum,chapName,prog):
 		super().__init__(master = parent)
 		rectangle_chapitre = tb.Canvas(mainFrame, width=rectangle_chapitreImg.width(), height=rectangle_chapitreImg.height())
 		rectangle_chapitre.create_image(0, 0, anchor=NW, image=rectangle_chapitreImg)
@@ -134,18 +148,18 @@ class Chapter(tb.Frame):
 		pourcentage.create_image(0, 0, anchor=NW, image=pourcentageImg)
 		pourcentage.pack(fill='y')
 		
-		tb.Label(chapitreFrame,text='0%',font=h3Font,background ='white',foreground='#002b36',justify="center").pack()
+		tb.Label(chapitreFrame,text=str(prog)+'%',font=h3Font,background ='white',foreground='#002b36',justify="center").pack()
 		
 		chapitreButton=Button(chapitreFrame)
-		if new==True: 
+		if prog==0: 
 			chapitreButton.configure(bg= "white",image=commencerImg)
 		else:
 			chapitreButton.configure(bg= "white",image=continuerImg)
 		chapitreButton.pack(pady = (30, 0))	
 			
-Chapter(mainFrame,1,1,'Introduction\n',True)
-Chapter(mainFrame,3,1,'Variables \net affectations',True)
-Chapter(mainFrame,5,1,'Arithmetique \net comparaisons',False)
+Chapter(mainFrame,1,1,chapitres[0],progressions[0])
+Chapter(mainFrame,3,1,chapitres[1],progressions[1])
+Chapter(mainFrame,5,1,chapitres[2],progressions[2])
 
 
 root.mainloop()
