fe99f050aff174d6762077399f0952c100535140
ppercent <70774607+ppercent@users.noreply.github.com>
2024-02-22 18:35:53 +0100

    Update markdown.py

--- sources/markdown.py
+++ sources/markdown.py
@@ -1,5 +1,5 @@
 import re       # Regular expression operations lib
-import tkinter as tk
+
 
 def findMarkdown(text: str) -> dict:
     patterns = {
@@ -36,53 +36,44 @@ def findIndex(detectedMarkdown: dict, originalText: str) -> dict:
 
 def sortMarkdown(detectedMarkdownIndex: dict) -> list:
     sortedMarkdown = []
-    sortedStartIndex = []
-
-    for index in detectedMarkdownIndex.values():
-        for currentIndex in index:
-            sortedStartIndex.append(currentIndex[0])
-    sortedStartIndex.sort()
+    combinedIndex = []
 
     for markdownType, index in detectedMarkdownIndex.items():
-        for i in range(len(sortedStartIndex)):
-            for currentIndex in index:
-                if sortedStartIndex[i] == index[0]:
-                    sortedMarkdown.append((markdownType, currentIndex[0], currentIndex[1]))
-
-# this function need to be fixed
-# this function need to be fixed
-
+        for start, end in index:
+            combinedIndex.append((markdownType, start, end))
+    
+    sorted_indices = sorted(combinedIndex, key=lambda x: (x[1], x[0]))
+    
+    for markdownType, start, end in sorted_indices:
+        sortedMarkdown.append((markdownType, start, end))
+    
     return sortedMarkdown
 
 
 def findIndexAfterFormat(detectedMarkdownIndex: dict) -> dict:
     indexAfterFormat = {}
     totalOffset = 0
+    sortedMarkdown = sortMarkdown(detectedMarkdownIndex)
 
-    for markdownType, index in detectedMarkdownIndex.items():
-        indexAfterFormat[markdownType] = []
-        for currentIndex in index:
-            if markdownType == 'bold':
-                indexAfterFormat[markdownType].append((currentIndex[0] - totalOffset - 2, currentIndex[1] - totalOffset - 2))
-                totalOffset += 4
-            else:
-                indexAfterFormat[markdownType].append((currentIndex[0] - totalOffset - 1, currentIndex[1] - totalOffset - 1))
-                totalOffset += 2
-    
+    for item in sortedMarkdown:
+        markdownType = item[0]
+        startIndex = item[1]
+        stopIndex = item[2]
+
+        if markdownType not in indexAfterFormat:
+            indexAfterFormat[markdownType] = []
+
+        if markdownType == 'bold':
+            indexAfterFormat[markdownType].append((startIndex - totalOffset - 2, stopIndex - totalOffset - 2))
+            totalOffset += 4
+        else:
+            indexAfterFormat[markdownType].append((startIndex - totalOffset - 1, stopIndex - totalOffset - 1))
+            totalOffset += 2
+
     return indexAfterFormat
 
 
-def setTextWidget(textWidget: tk.Text, text: str):
-    # find index of the detected markdown in the text
-    detectedMarkdown = findMarkdown(text)
-    detectedMarkdownIndex = findIndex(detectedMarkdown, text)
-
-    # default paragraph font
-    pFont = ('Inter', 15)
-
-    # configure the custom tags
-    textWidget.tag_config('bold_text', font=(pFont[0], pFont[1], 'bold'))
-    textWidget.tag_config('italic_text', font=(pFont[0], pFont[1], 'italic'))
-    textWidget.tag_config('underline_text', font=(pFont[0], pFont[1], 'underline'))
-    textWidget.tag_config('code_text', font=(pFont[0], pFont[1], 'bold'), background='#383e4a', foreground='#4794af')
-    # to be continued
+# todo: 
+# def setTextWidget(textWidget: tk.Text, text: str):
+# this function should do all of the formatting job
+# maybe create a function to detect where the line is located
